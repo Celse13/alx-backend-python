cb37ad78e3d0d16acaa30a51f40aa5e508fe44c1
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-18 21:01:57 +0200

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-""" Async """
+""" Async function """
 import asyncio
 import random
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ Async """
-    wait = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return wait
+async def wait_n(n: int, max_delay: int) -> list:
+    """ wait n """"
+    delays = []
+    for _ in range(n):
+        delay = asyncio.create_task(wait_random(max_delay))
+        delays.append(delay)
+    completed, pending = await asyncio.wait(delays, return_when=asyncio.ALL_COMPLETED)
+    return [task.result() for task in completed]
