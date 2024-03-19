2456a9e7bfcbe42220a39de6e6cb8cc8c1d879c5
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-19 20:07:50 +0200

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,11 @@
 """ Async """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """ Async """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
