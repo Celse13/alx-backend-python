253ce4949d60e3014b443c597ef19d5a27c8056a
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-03-19 20:18:04 +0200

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-""" Async func """
-import asyncio
-import random
-from typing import Generator
+""" Async """
+from typing import List
+
+async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """ Async """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+    return [num async for num in async_generator()]
